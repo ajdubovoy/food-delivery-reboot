69c42d574e3eace7eec8fda37cd9d3af7bfffaa8
Alexander Dubovoy <ajdubovoy@gmail.com>
2022-08-03 15:00:17 +0200

    add app and router

--- app.rb
+++ app.rb
@@ -1,1 +1,10 @@
-# TODO: Glue together all the files so this file will start your app
+# app.rb
+require_relative "app/repositories/restaurant_repository"
+require_relative "app/controllers/restaurants_controller"
+restaurant_repository = RestaurantRepository.new("data/restaurants.csv")
+restaurants_controller = RestaurantsController.new(restaurant_repository)
+
+require_relative "router"
+router = Router.new(restaurants_controller)
+
+router.run
--- router.rb
+++ router.rb
@@ -1,1 +1,30 @@
-# TODO: code a class `Router` that shows the user the menu of options and asks the user to choose an option.
+# router.rb
+class Router
+  def initialize(restaurants_controller)
+    @restaurants_controller = restaurants_controller
+    @running = true
+  end
+
+  def run
+    # should start the whole application
+    while @running
+      print_menu
+      user_choice = gets.chomp.to_i
+      route_action(user_choice)
+    end
+  end
+
+  private
+
+  def print_menu
+    puts "1. List all restaurants"
+    puts "9. Quit"
+  end
+
+  def route_action(user_choice)
+    case user_choice
+    when 1 then @restaurants_controller.list
+    when 9 then @running = false
+    end
+  end
+end
